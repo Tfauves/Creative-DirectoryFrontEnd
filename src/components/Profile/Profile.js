2343f90e1b8683ed24646e0412e2481c391e37a9
Tranquility Base <tfauvelle@gmail.com>
2022-06-12 21:12:10 -0400

    profile is being brought in, small bug of getting multi objs

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,86 @@
+import React, { useState, useEffect, useContext } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { AuthContext } from "../Providers/AuthProvider";
+import { apiHost } from "../../config";
+import Spinner from "../faCommon/Spinner";
+import { Container } from "@mui/system";
+import { Fragment } from "react";
+
+const Profile = (props) => {
+  const params = useParams();
+  const [owner, setOwner] = useState({
+    id: params.proId,
+  });
+  const [loading, setLoading] = useState(true);
+  const [auth] = useContext(AuthContext);
+
+  useEffect(() => {
+    const _fetchOwner = async () => {
+      const res = await axios.get(`${apiHost}/api/profile/${owner.id}`, {
+        headers: {
+          Authorization: `Bearer ${auth.token}`,
+        },
+      });
+      console.log(res.data);
+      setOwner(res.data);
+      setLoading(false);
+    };
+    setLoading(true);
+    _fetchOwner();
+  }, []);
+
+  const displayProfile = () => {
+    return (
+      <Fragment>
+        <div style={{ marginTop: "3em" }}>
+          <div
+            style={{
+              fontFamily: "Bebas Neue, cursive",
+              flex: 2,
+              flexDirection: "column",
+              color: "#303030",
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <h1>{owner.fname.toUpperCase()}</h1>
+          </div>
+        </div>
+        <Container>
+          <div>
+            <h1
+              style={{
+                fontFamily: "Bebas Neue, cursive",
+                marginTop: "1em",
+                textAlign: "center",
+                fontSize: "3em",
+                letterSpacing: "13px",
+                marginBottom: "1em",
+                textDecorationLine: "underline",
+              }}
+            >
+              Artist Media
+            </h1>
+            <h2>{owner.media.type}</h2>
+          </div>
+        </Container>
+      </Fragment>
+    );
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flex: "1",
+        flexDirection: "column",
+        alignItems: "center",
+        minHeight: "100vh",
+      }}
+    >
+      {loading ? <Spinner /> : displayProfile()}
+    </div>
+  );
+};
+export default Profile;
